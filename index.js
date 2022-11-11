deaff7979c5ba128e551a356d4334eaee4b22366
youngbloodcyb <cameron.youngblood@gmail.com>
2022-11-11 07:57:54 -0700

    add button and make function callback

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-let now = new Date().getTime();
-const future = now + (1000 * 60 * 20);
+const timer = () => {
+    let now = new Date().getTime();
+    const future = now + (1000 * 60 * 20);
 
-let x = setInterval(() => {
-    now = new Date().getTime();
-    let distance = future - now;
+    let x = setInterval(() => {
+        now = new Date().getTime();
+        let distance = future - now;
 
-    let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-    document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
+        document.getElementById("timer").innerHTML = minutes + "m " + seconds + "s ";
 
-    if (distance < 0) {
-        clearInterval(x);
-        document.getElementById("timer").innerHTML = "Look away!";
-        window.alert("Look away!");
-    }
-}, 1000);
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("timer").innerHTML = "Look away!";
+            window.alert("Look away!");
+        }
+    }, 1000);
+}
